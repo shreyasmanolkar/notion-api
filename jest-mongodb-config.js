d49f6f2ffc9c5a246dd4ef28489021656f6985c3
shreyas <shreyasmanolkar123@gmail.com>
2023-05-13 15:24:39 +0530

    chore: created initial project setup

---
+++ jest-mongodb-config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '5.2.0',
+      skipMD5: true,
+    },
+    autoStart: false,
+    instance: {},
+  },
+};
