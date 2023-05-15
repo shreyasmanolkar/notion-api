19dee3d0584d32e1f620b0b64de4122949c22d1d
shreyas <shreyasmanolkar123@gmail.com>
2023-05-15 21:11:20 +0530

    test(application): added mocks for usecase implementations

---
+++ tests/application/mocks/users/use-cases.ts
@@ -0,0 +1,130 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { AddPageIdToFavoritesByWorkspaceIdInterface } from '@application/interfaces/use-cases/users/AddPageIdToFavoritesByWorkspaceIdInterface';
+import { AddWorkspaceByUserIdInterface } from '@application/interfaces/use-cases/users/AddWorkspaceByUserIdInterface';
+import { AuthenticateInterface } from '@application/interfaces/use-cases/users/AuthenticateInterface';
+import { DeleteUserInterface } from '@application/interfaces/use-cases/users/DeleteUserInterface';
+import { GetFavoritesByWorkspaceIdInterface } from '@application/interfaces/use-cases/users/GetFavoritesByWorkspaceIdInterface';
+import { GetUserByIdInterface } from '@application/interfaces/use-cases/users/GetUserByIdInterface';
+import { GetWorkspacesByUserIdInterface } from '@application/interfaces/use-cases/users/GetWorkspacesByUserIdInterface';
+import { RemovePageIdFromFavoritesByWorkspaceIdInterface } from '@application/interfaces/use-cases/users/RemovePageIdFromFavoritesByWorkspaceIdInterface';
+import { RemoveWorkspaceByUserIdInterface } from '@application/interfaces/use-cases/users/RemoveWorkspaceByUserIdInterface';
+import { SignInInterface } from '@application/interfaces/use-cases/users/SignInInterface';
+import { SignUpInterface } from '@application/interfaces/use-cases/users/SignUpInterface';
+import { UpdateUserInterface } from '@application/interfaces/use-cases/users/UpdateUserInterface';
+import { UpdateUserProfilePictureInterface } from '@application/interfaces/use-cases/users/UpdateUserProfilePictureInterface';
+import mockUser from '@tests/domain/mock-user';
+
+export class AuthenticateStub implements AuthenticateInterface {
+  async execute(
+    _authenticationToken: string
+  ): Promise<AuthenticateInterface.Response> {
+    return 'sample_token';
+  }
+}
+
+export class SignInStub implements SignInInterface {
+  async execute(
+    _credentials: SignInInterface.Request
+  ): Promise<SignInInterface.Response> {
+    return 'sample-jwt-token';
+  }
+}
+
+export class SignUpStub implements SignUpInterface {
+  async execute(
+    _userData: SignUpInterface.Request
+  ): Promise<SignUpInterface.Response> {
+    const { id } = mockUser();
+    return id;
+  }
+}
+
+export class GetFavoritesByWorkspaceIdStub
+  implements GetFavoritesByWorkspaceIdInterface
+{
+  async execute(
+    _params: GetFavoritesByWorkspaceIdInterface.Request
+  ): Promise<GetFavoritesByWorkspaceIdInterface.Response> {
+    return ['sample-page-1'];
+  }
+}
+
+export class GetUserByIdStub implements GetUserByIdInterface {
+  async execute(
+    _userId: GetUserByIdInterface.Request
+  ): Promise<GetUserByIdInterface.Response> {
+    const user = mockUser();
+    return user;
+  }
+}
+
+export class GetWorkspacesByUserIdStub
+  implements GetWorkspacesByUserIdInterface
+{
+  async execute(
+    _userId: GetWorkspacesByUserIdInterface.Request
+  ): Promise<GetWorkspacesByUserIdInterface.Response> {
+    return [
+      {
+        workspaceId: 'sample-workspace-id',
+        favorites: ['sample-page-id'],
+      },
+    ];
+  }
+}
+
+export class AddPageIdToFavoritesByWorkspaceIdStub
+  implements AddPageIdToFavoritesByWorkspaceIdInterface
+{
+  async execute(
+    _params: AddPageIdToFavoritesByWorkspaceIdInterface.Request
+  ): Promise<AddPageIdToFavoritesByWorkspaceIdInterface.Response> {}
+}
+
+export class AddWorkspaceByUserIdStub implements AddWorkspaceByUserIdInterface {
+  async execute(
+    _params: AddWorkspaceByUserIdInterface.Request
+  ): Promise<AddWorkspaceByUserIdInterface.Response> {}
+}
+
+export class UpdateUserStub implements UpdateUserInterface {
+  async execute(
+    _params: UpdateUserInterface.Request
+  ): Promise<UpdateUserInterface.Response> {
+    const user = mockUser();
+    return user;
+  }
+}
+
+export class UpdateUserProfilePictureStub
+  implements UpdateUserProfilePictureInterface
+{
+  async execute(
+    params: UpdateUserProfilePictureInterface.Request
+  ): Promise<void> {}
+}
+
+export class RemovePageIdFromFavoritesByWorkspaceIdStub
+  implements RemovePageIdFromFavoritesByWorkspaceIdInterface
+{
+  async execute(
+    _params: RemovePageIdFromFavoritesByWorkspaceIdInterface.Request
+  ): Promise<RemovePageIdFromFavoritesByWorkspaceIdInterface.Response> {}
+}
+
+export class RemoveWorkspaceByUserIdStub
+  implements RemoveWorkspaceByUserIdInterface
+{
+  async execute(
+    _params: RemoveWorkspaceByUserIdInterface.Request
+  ): Promise<RemoveWorkspaceByUserIdInterface.Response> {}
+}
+
+export class DeleteUserStub implements DeleteUserInterface {
+  async execute(
+    _userId: DeleteUserInterface.Request
+  ): Promise<DeleteUserInterface.Response> {}
+}
