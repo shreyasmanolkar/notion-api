5197c35701891ec8972d7fd7359d4cf584c48a6c
shreyas <shreyasmanolkar123@gmail.com>
2023-05-26 16:37:01 +0530

    feat(domain): added doamin entity for page

---
+++ src/domain/entities/Page.ts
@@ -0,0 +1,71 @@
+export type PageSettingsType = {
+  font: 'sans-serif' | 'serif' | 'monospace';
+  smallText: boolean;
+  fullWidth: boolean;
+  lock: boolean;
+};
+
+export type CoverPictureType = {
+  url: string;
+};
+
+export type ContentType = {
+  type: string;
+  content: unknown[];
+};
+
+export type PageProps = {
+  id: string;
+  reference: string;
+  title: string;
+  icon: string;
+  coverPicture: CoverPictureType;
+  content: ContentType;
+  favorite: string[];
+  pageSettings: PageSettingsType;
+  path: string;
+  workspaceId: string;
+  createdAt: Date;
+  updatedAt?: Date;
+};
+
+export class User {
+  public readonly id: string;
+
+  public readonly reference: string;
+
+  public readonly title: string;
+
+  public readonly icon: string;
+
+  public readonly coverPicture: CoverPictureType;
+
+  public readonly content: ContentType;
+
+  public readonly favorite: string[];
+
+  public readonly pageSettings: PageSettingsType;
+
+  public readonly path: string;
+
+  public readonly workspaceId: string;
+
+  public readonly createdAt: Date;
+
+  public readonly updatedAt?: Date;
+
+  constructor(props: PageProps) {
+    this.id = props.id;
+    this.reference = props.reference;
+    this.title = props.title;
+    this.icon = props.icon;
+    this.coverPicture = props.coverPicture;
+    this.content = props.content;
+    this.favorite = props.favorite;
+    this.pageSettings = props.pageSettings;
+    this.path = props.path;
+    this.workspaceId = props.workspaceId;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
+  }
+}
