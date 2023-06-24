728eb7dc19deafdd1eb3f8ce79e1452aefe94d25
shreyas <shreyasmanolkar123@gmail.com>
2023-06-24 16:16:44 +0530

    refactor(workspace): added flow to remove Page metadata from workspace

--- src/main/factories/controllers/pages/delete-page/controller-factory.ts
+++ src/main/factories/controllers/pages/delete-page/controller-factory.ts
@@ -2,10 +2,16 @@ import { BaseController } from '@infrastructure/http/controllers/BaseController'
 import { DeletePageController } from '@infrastructure/http/controllers/pages/DeletePageController';
 import { makeDeletePage } from '@main/factories/use-cases/pages/delete-page-factory';
 import { makeGetPageById } from '@main/factories/use-cases/pages/get-page-by-id-factory';
+import { makeRemovePageByPageId } from '@main/factories/use-cases/workspaces/remove-page-by-page-id-factory';
 
 export const makeDeletePageController = (): BaseController => {
   const getPageByIdUseCase = makeGetPageById();
   const deletePageUseCase = makeDeletePage();
+  const removePageByPageIdUseCase = makeRemovePageByPageId();
 
-  return new DeletePageController(getPageByIdUseCase, deletePageUseCase);
+  return new DeletePageController(
+    getPageByIdUseCase,
+    deletePageUseCase,
+    removePageByPageIdUseCase
+  );
 };
