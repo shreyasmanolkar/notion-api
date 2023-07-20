231844eb95aeb96c447467d5ed7c14bbf0e62a72
shreyas <shreyasmanolkar123@gmail.com>
2023-07-21 04:05:00 +0530

    feat(env): updated file to get env variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 export default {
   port: process.env.PORT || 5000,
   mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/notion',
-  // TODO: add access and refresh tokens
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || 'accesstokensecret',
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || 'refereshtokensecret',
   bcryptSalt: 10,
